c25044cbe11c4bc51e5ba58e47e0c644bcbbc33c
Hannah Byrne <S00189764@mail.itsligo.ie>
2019-11-07 11:21:21 +0000

    Added Student Class

---
+++ CA1Question/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1Question
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+
+        public List<Subject> Subjects;
+
+        public Student(string name, string dateofbirth)
+        {
+            Name = name;
+            DateOfBirth = dateofbirth;
+        }
+    }
+}
